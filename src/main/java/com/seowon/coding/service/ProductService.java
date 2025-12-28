9c4c4c1974a4117111f8d0ff1eade348cfc63c00
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-12-28 15:12:01 +0900

    fix : TODO#1

--- src/main/java/com/seowon/coding/service/ProductService.java
+++ src/main/java/com/seowon/coding/service/ProductService.java
@@ -51,7 +51,9 @@ public class ProductService {
     public List<Product> findProductsByCategory(String category) {
         // TODO #1: 구현 항목
         // Repository를 사용하여 category 로 찾을 제품목록 제공
-        return List.of();
+
+        // ProductRepository의 findByCategory(category)를 사용하여 리턴
+        return productRepository.findByCategory(category);
     }
 
     /**
